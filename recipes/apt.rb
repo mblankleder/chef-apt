370f4f5ca271a3ec21e6667378d71523b0fae16b
Michel Blankleder <michel.blankleder@gmail.com>
2014-09-07 08:01:42 -0300

    added apt cookbook option

---
+++ recipes/apt.rb
@@ -0,0 +1,10 @@
+execute "apt-get-update-periodic" do
+  command "apt-get update"
+  ignore_failure true
+  only_if do
+    File.exists?('/var/lib/apt/periodic/update-success-stamp') &&
+    File.mtime('/var/lib/apt/periodic/update-success-stamp') < Time.now - 86400
+  end
+end
+
+include_recipe "apt::default"
